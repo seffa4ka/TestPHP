924b992d920f38886ab8d07c16b4c139f01cfaa8
seffa4ka <th-seffka@mail.ru>
2017-04-16 18:55:54 +0600

    Added CSS and JS.

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-  echo 'Hello, World!';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link href="css/global.css" rel="stylesheet">
+</head>
+<body>
+  <script src="js/common.js"></script>
+  <main>
+    <p>
+      <?php
+        echo 'Hello, World!';
+      ?>
+    </p>
+  </main>
+</body>
+</html>
